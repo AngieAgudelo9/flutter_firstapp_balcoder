ab706210a1bae15dd4397593b5f2998005951f55
AngieAgudelo9 <agudeloangie10c3@gmail.com>
2021-02-14 19:22:12 -0500

    MediaQuery y bools

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,8 @@ class MyApp extends StatelessWidget {
 
 
 
+
+
 // class MyApp extends StatelessWidget {
 //   // This widget is the root of your application.
 //   @override
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -7,9 +7,10 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   // TIPO VARIABLE + NOMBRE DE MI VARIABLE  + INICIALIZAR (=)
+  // BOLL: Falso/Verdadero
   String title = "Este es el título";
   int count = 0;
-  bool isRed = false;
+  bool mybool = true;
   double numDecimal = 0.5;
 
   @override
@@ -22,6 +23,13 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+//MEDIAQUERY: IDENTIFICA EL TAMAÑO DEL DISPOSITIVO (HEIGHT:ALTO,WIDTH:ANCHO)
+//
+    double _height = MediaQuery.of(context).size.height;
+    double _width = MediaQuery.of(context).size.width;
+
+    print(_height);
+    print(_width);
     return Scaffold(
       appBar: AppBar(
           backgroundColor: Colors.cyan,
@@ -30,22 +38,51 @@ class _HomePageState extends State<HomePage> {
         child: Column(
           children: [
             Spacer(),
-            Text("El contador va en: ", style: TextStyle (fontSize: 32.0)),
+            Text("El contador va en: ", style: TextStyle(fontSize: 32.0)),
             Center(
                 child: Text(
               count.toString(),
-              style: TextStyle(fontSize: 32.0, fontWeight: FontWeight.w800),
+              //FontWeight: grosor de la fuente.
+              style: TextStyle(
+                  fontSize: 32.0,
+                  fontWeight: FontWeight.w800,
+                  color: Colors.yellow),
             )),
-            Container(height: 100, color: Colors.yellow, child: Row(
-              children: 
-              [Spacer (),
-              Container( color: Colors.green,
-              width: 50,
-              )],)),
+
+            //SPACER: ESPACIO CONTAINER:
+
+            GestureDetector(
+              onTap: () {
+                print("lo undimos)");
+
+//!mybool: Intermediación me sirve para cambios
+                setState(() {
+                                  mybool = !mybool;
+
+                });
+
+
+
+
+              },
+              child: Container(
+                width: _width * 0.8,
+                height: _height * 0.1,
+                color: Colors.yellow,
+              ),
+            ),
+
             Container(
-              child: 
-              Spacer()),
-              Container( color: Colors.lime)
+                width: _width * 0.5,
+                height: _height * 0.3,
+                //Estoy aplicando una condicional, donde le digo true/false de unos colores en específico. 
+                color: mybool ? Colors.green:  Colors.red,
+                child: Icon(
+                  Icons.calculate,
+                  color: Colors.yellow,
+                  size: 40,
+                )),
+            Spacer()
           ],
         ),
       ),
